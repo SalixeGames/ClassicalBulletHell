e16db2d6c2fe573b8131b5a88340ebcecce1849a
Salixe1214 <salixegames@gmail.com>
2024-10-15 12:38:06 -0400

    + some shits

--- hero.gd
+++ hero.gd
@@ -1,8 +1,7 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const SPEED = 750.0
 
 
 func _physics_process(delta: float) -> void:
@@ -15,5 +14,6 @@ func _physics_process(delta: float) -> void:
 		velocity.y = direction.y * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.y = move_toward(velocity.y, 0, SPEED)
 
 	move_and_slide()
